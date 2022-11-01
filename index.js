fc282e05f81cbad9ae9538e753963fa9cf4b8b06
Cameron Ward <cameronuward@gmail.com>
2022-11-01 10:04:39 -0700

    add function to create dropdown

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
+let opts = {};
+
 function setup() {
-    createCanvas(400, 900);
+    createCanvas(800, 900);
 
-    textAlign(CENTER);
-    background(190, 204, 100);
-    sel = createSelect();
-    sel.position(100, 100);
-    sel.option('Select Mode');
-    sel.option('Direction');
-    sel.option('Colour');
-    sel.option('Typing');
-    sel.changed(mySelectEvent);
+    createDropdown('Mode', [100, 100], ['Direction', 'Colour', 'Typing']);
+    createDropdown('Difficulty', [235, 100], ['Easy', 'Medium', 'Hard']);
+    createDropdown('No. of Rounds', [390, 100], ['2', '4', '6']);
+}
 
+function createDropdown(id, pos, opts) {
+    const text = opts[id] = `Select ${id}`;
     textAlign(CENTER);
-    background(190, 204, 100);
-    sel = createSelect();
-    sel.position(200, 100);
-    sel.option('Select Difficulty');
-    sel.option('Easy');
-    sel.option('Medium');
-    sel.option('Hard');
-    sel.changed(mySelectEvent);
 
-    textAlign(CENTER);
-    background(190, 204, 100);
     sel = createSelect();
-    sel.position(300, 100);
-    sel.option('Select No. of Rounds');
-    sel.option('2');
-    sel.option('4');
-    sel.option('6');
-    sel.changed(mySelectEvent);
+    sel.position(pos[0], pos[1]);
+    sel.changed(() => {
+        const val = sel.value();
+        opts[id] = val === text ? null : val;
+        console.log(`dropdown '${id}' set to '${val}'`);
+    });
+    
+    for (const opt of [text, ...opts]) {
+        sel.option(opt);
+    }
 }
 
 function draw() {
     colorMode(HSB);
     background(190, 204, 100);
-}
-
-function mySelectEvent() {
-    let item = sel.value();
-    background(190, 204, 100);
-    text('It is a ' + item + '!', 50, 50);
 }
