13903ccde750dd209d9a736b809e32fc1216f933
ekagra1602 <117231084+ekagra1602@users.noreply.github.com>
2022-11-21 20:32:01 -0700

    Update sketch.js
    Added prompt for typing

--- sketch.js
+++ sketch.js
@@ -1,20 +1,32 @@
 p5.disableFriendlyErrors = true;
-var cnv;
+var cnv, opts = {}, state, backgroundImg, settingsImg;
+let img3;
+function preload() {
+  img3 = loadImage('assets/good.png');
+}
 
 function setup() {
     cnv = createCanvas(650, 600);
     colorMode(HSB, 360, 100, 100, 100);
-
-    initMenu();
+    strokeJoin(ROUND);
+    setState(STATE.MENU);
 }
 
 function draw() {
+    background(backgroundImg);
+
     switch (state) {
         case STATE.MENU:
             drawMenu();
             break;
         case STATE.COLOR:
             drawColor();
+            break;
+        case STATE.DIRECTION:
+            drawDirection();
+            break;
+        case STATE.TYPING:
+            drawTyping();
             break;
         default:
             background(0, 100, 100);
@@ -23,8 +35,6 @@ function draw() {
 }
 
 // #region state management
-var state;
-
 const STATE = {
     MENU: "Menu",
     DIRECTION: "Direction",
@@ -33,30 +43,50 @@ const STATE = {
 };
 
 function setState(newState) {
-    if (state === STATE.MENU)
-        menuChildren().map(el => el.remove());
+    background(0);
 
-    if (newState == STATE.MENU)
-        initMenu();
+    cnv.mousePressed(false);
+    cnv.mouseReleased(false);
+
+    if (opts.elements)
+        opts.elements.map((el) => el.remove());
+
+    if (newState !== STATE.MENU) {
+        let btn = createButton("Menu");
+        btn.position(0, 0);
+        btn.size(60, 30);
+        btn.mousePressed(() => setState(STATE.MENU));
+        opts.elements = [btn];
+    } else opts.elements = [];
+
+    switch (newState) {
+        case STATE.MENU:
+            initMenu();
+            break;
+        case STATE.COLOR:
+            initColor();
+            break;
+        case STATE.DIRECTION:
+            initDirection();
+            break;
+        case STATE.TYPING:
+            initTyping();
+            break;
+    }
 
     state = newState;
 }
 // #endregion
 
 // #region menu
-var opts, backgroundImg, settingsImg;
-
 function initMenu() {
-    opts = ({
-        settings: false
-    });
+    opts = ({});
     state = STATE.MENU;
-    backgroundImg = loadImage("assets/background.jpg");
-    settingsImg = loadImage("assets/settings.png");
+    backgroundImg = loadImage("assets/menu_bg.jpg");
 
     const labels = ["Mode", "Difficulty", "# Rounds"];
-    const modes = Object.values(STATE).filter(v => v !== STATE.MENU);
-    opts.dropdowns = [
+    const modes = Object.values(STATE).filter((v) => v !== STATE.MENU);
+    const dropdowns = [
         createDropdown(labels[0], [110, 300], modes),
         createDropdown(labels[1], [250, 300], ["Easy", "Medium", "Hard"]),
         createDropdown(labels[2], [400, 300], ["2", "4", "6"]),
@@ -67,39 +97,14 @@ function initMenu() {
     start.size(100, 50);
     start.style("background", color(25, 23, 100));
     start.mousePressed(function () {
-        const [mode, diff, rounds] = labels.map(k => opts[k]);
-        if (mode && diff && rounds)
-            setState(mode);
+        const [mode, diff, rounds] = labels.map((k) => opts[k]);
+        if (mode && diff && rounds) setState(mode);
     });
 
-    // settings button
-    const settings = createImg("assets/settings.png");
-    settings.position(610, 0);
-    settings.size(40, 40);
-    settings.mousePressed(function () { // when the gear is clicked
-        opts.settings = true; // open the settings menu
-        menuChildren().map(el => el.hide()); // hide the dropdowns & btns
-        cnv.mousePressed(function () { // register a click handler
-            const [outX, outY] = [mouseX < 50 || mouseX > 600, mouseY < 50 || mouseY > 550];
-            if (outX || outY) { // when the click is outside the settings menu
-                opts.settings = false; // close the settings menu
-                menuChildren().map(el => el.show()); // show the underlying elements
-                cnv.mousePressed(false); // unregister the click handler
-            }
-        })
-    })
-
-    opts.btns = [start, settings];
+    opts.elements = [start, ...dropdowns];
 }
 
 function drawMenu() {
-    background(backgroundImg);
-
-    if (opts.settings) {
-        drawSettings();
-        return;
-    }
-
     fill("pink");
     stroke("black");
     strokeWeight(8);
@@ -110,24 +115,6 @@ function drawMenu() {
     textAlign(CENTER);
     text("Wonder Kids", 325, 180);
 }
-
-function drawSettings() {
-    noStroke();
-    fill(0, 0, 0, 40);
-    rect(0, 0, 650, 600);
-
-    fill(210, 23, 40, 96);
-    rect(50, 50, 550, 500);
-
-    textSize(40);
-    textStyle(NORMAL);
-    fill("pink");
-    stroke("black");
-    strokeWeight(8);
-    text("Settings", 325, 120);
-}
-
-const menuChildren = () => [...opts.btns, ...opts.dropdowns];
 
 function createDropdown(id, pos, entries) {
     const text = `Select ${id}`;
@@ -150,18 +137,301 @@ function createDropdown(id, pos, entries) {
 // #endregion
 
 // #region color page
-function drawColor() {
-    background(240, 100, 100);
+function initColor() {
+    backgroundImg = loadImage("assets/color_bg.jpg");
 
-    text(`Select the color ${color}`);
-    const colors = ["red", "yellow", "blue", "green"];
+    const colorBank = {
+        Easy: ["red", "yellow", "blue", "green"],
+        Medium: ["red", "yellow", "blue", "green", "orange", "purple"],
+        Hard: ["red", "yellow", "blue", "green", "orange", "purple", "pink"],
+    };
+
+    opts.bank = colorBank[opts["Difficulty"]]
+        .sort(() => 0.5 - Math.random())
+        .slice(0, 4);
+
+    const index = Math.floor(Math.random() * 4);
+    opts.color = opts.bank[index];
+
+    cnv.mousePressed(function () {
+        if (opts.showResults)
+            if (--opts["# Rounds"]) {
+                // dismiss results screen
+                opts.showResults = false;
+                opts.bank = colorBank[opts["Difficulty"]]
+                    .sort(() => 0.5 - Math.random())
+                    .slice(0, 4);
+
+                const index = Math.floor(Math.random() * 4);
+                opts.color = opts.bank[index];
+            } // no rounds left
+            else setState(STATE.MENU);
+        else {
+            // user maybe clicked a square
+            for (let i = 0; i < 4; i++) {
+                const [width, height, pad] = [200, 200, 10];
+                const x = i % 2 ? 325 + pad : 325 - width - pad;
+                const y = 40 + (Math.floor(i / 2) ? 300 + pad : 300 - height - pad);
+                const mouseOut =
+                    mouseX < x || mouseX > x + width || mouseY < y || mouseY > y + height;
+
+                if (!mouseOut) {
+                    // they clicked this square
+                    const correct = opts.color === opts.bank[i];
+                    const score = opts.results ? opts.results.score + correct : correct;
+                    const splashes = [
+                        "Nice one!",
+                        "Great job!",
+                        "Good work!",
+                        "That's right!",
+                        "Correct!",
+                        "Spot on!",
+                    ];
+                    const splash = correct
+                        ? splashes[Math.floor(Math.random() * splashes.length)]
+                        : "Wrong!";
+
+                    opts.results = {
+                        correct,
+                        score,
+                        splash,
+                        index: i,
+                    };
+                    opts.showResults = true;
+                }
+            }
+        }
+    });
+}
+
+function drawColor() {
+    if (opts.showResults) {
+        const { correct, score, index, splash } = opts.results;
+
+        stroke("black");
+        strokeWeight(8);
+        textSize(52);
+        textFont("Segoe UI");
+        fill(correct ? "white" : "red");
+        fill("pink");
+        stroke("black");
+        strokeWeight(8);
+        text(splash, 300, 100);
+      if(correct){
+        
+        image(img3, 130, 150, 300, 300);
+      }
+
+        if (!correct) {
+            textSize(24);
+            text(`You picked ${opts.bank[index]}, not ${opts.color}`, 300, 220);
+
+            for (let i = 0; i < 2; i++) {
+                const [width, height, pad] = [200, 200, 10];
+                const x = i % 2 ? 325 + pad : 325 - width - pad;
+                fill(i ? opts.color : opts.bank[index]);
+                stroke(i ? "green" : "red");
+                rect(x, 300, width, height);
+            }
+        }
+
+        stroke("black");
+        strokeWeight(4);
+        fill("white");
+        textSize(25);
+        fill("pink");
+        stroke("black");
+        strokeWeight(8);
+        text("Click Anywhere to Continue", 300, 550);
+
+        return;
+    }
 
     for (let i = 0; i < 4; i++) {
-        const x = (i % 2) * 325;
-        const y = Math.floor(i / 2) * 300;
-        
-        fill(colors[i]);
-        rect(x, y, 325, 300);
+        const [width, height, pad] = [200, 200, 10];
+        const x = i % 2 ? 325 + pad : 325 - width - pad;
+        const y = 40 + (Math.floor(i / 2) ? 300 + pad : 300 - height - pad);
+        const mouseOut =
+            mouseX < x || mouseX > x + width || mouseY < y || mouseY > y + height;
+
+        fill(opts.bank[i]);
+        stroke(mouseOut ? opts.bank[i] : "white");
+        rect(x, y, width, height);
+    }
+
+    stroke("black");
+    strokeWeight(8);
+    fill("white");
+    textSize(52);
+    textFont("Segoe UI");
+    fill("pink");
+    stroke("black");
+    strokeWeight(8);
+    text(`Select the color ${opts.color}`, 325, 80);
+}
+// #endregion
+
+// #region direction page
+function initDirection() {
+    backgroundImg = loadImage("assets/dir_bg.jpg");
+
+    const dirBank = {
+        "Easy": ["Right", "Up", "Left", "Down"],
+        "Medium": ["East", "North", "West", "South"],
+        "Hard": ["East", "North", "West", "South"],
+    };
+
+    opts.direction = dirBank[opts["Difficulty"]][Math.floor(Math.random() * 4)];
+    cnv.mousePressed(function () {
+        if (opts.showResults) {
+            if (--opts["# Rounds"]) {
+                opts.showResults = false;
+                opts.direction = dirBank[opts["Difficulty"]][Math.floor(Math.random() * 4)];
+            } else setState(STATE.MENU);
+        } else {
+            const mouse = createVector(mouseX, mouseY);
+            if (createVector(350, 300).sub(mouse).mag() <= 100)
+                opts.mouseStart = mouse;
+        }
+    });
+    cnv.mouseReleased(function () {
+        if (!opts.mouseStart) return;
+        let mouse = opts.mouseStart.sub(mouseX, mouseY);
+        if (mouse.mag() > 100) {
+            switch (Math.floor(4 * mouse.heading() / PI)) {
+                case 3:
+                case -4:
+                    opts.dir = 0; // right
+                    break;
+                case -3:
+                case -2:
+                    opts.dir = 3; // down
+                    break;
+                case 0:
+                case -1:
+                    opts.dir = 2; // left
+                    break;
+                case 1:
+                case 2:
+                    opts.dir = 1; // up
+                    break;
+            }
+            opts.results = opts.dir === dirBank[opts["Difficulty"]].indexOf(opts.direction);
+            opts.showResults = true;
+        }
+        opts.mouseStart = undefined;
+    });
+}
+
+function drawDirection() {
+
+    stroke("black");
+    strokeWeight(8);
+    fill("white");
+    textSize(52);
+    textFont("Segoe UI");
+    fill("pink");
+    stroke("black");
+
+    if (opts.showResults) {
+        if(opts.results==true){
+  image(img3, 130, 150, 300, 300);
+}
+        text(opts.results ? "Great job!" : "Wrong!", 325, 90);
+        text("Click to continue...", 325, 550);
+        return;
+    }
+    text(`Move the circle ${opts.direction}`, 325, 80);
+
+    noStroke();
+    if (opts.mouseStart) {
+        fill("red");
+        circle(mouseX, mouseY, 100);
+    } else {
+        fill("yellow");
+        circle(320, 300, 100);
     }
 }
 // #endregion
+
+// #region typing page
+function initTyping() {
+    backgroundImg = loadImage("assets/kbd_bg.jpg");
+
+    const wordBank = {
+        "Easy": "abcdefghijklmnopqrstuvwxyz".split(""),
+        "Medium": ["ace", "act", "add", "age", "ago", "aid", "aim", "air", "ale", "all", "and", "ant", "any", "ape", "app", "apt", "arc", "are", "arm", "art", "ash", "ask", "ate", "awe", "axe", "bar", "bat", "cab", "can", "cap", "car", "cat", "cel", "cob", "cod", "cog", "cop", "cot"],
+        "Hard": ["lamp", "dark", "pace", "here", "this", "that", "what", "come", "came", "down", "post", "burn", "hate", "crib", "milk", "numb", "grid", "game", "goat", "beat", "brat", "lion", "plan", "plum", "peer", "pear"],
+    };
+    const bank = wordBank[opts["Difficulty"]];
+    opts.word = bank[Math.floor(Math.random() * bank.length)];
+    opts.showResults = false;
+    opts.buffer = "";
+
+    cnv.mousePressed(function () {
+        if (!opts.showResults) return;
+
+        if (--opts["# Rounds"]) initTyping();
+        else setState(STATE.MENU);
+    });
+}
+
+function drawTyping() {
+    if (opts.showResults) {
+        textFont("Georgia");
+        textAlign(CENTER);
+        textSize(40);
+        fill("pink");
+        stroke("black");
+        strokeWeight(8);
+        text("Great Work!", 325, 90);
+        text("Click to Continue", 325, 550);
+       image(img3, 130, 150, 300, 300);
+        return;
+    }
+    textFont("Georgia");
+    textSize(52);
+    textAlign(LEFT);
+    const longer = opts.word.length > opts.buffer.length ? opts.word : opts.buffer;
+   textFont("Georgia");
+        textAlign(CENTER);
+        textSize(35);
+        fill("pink");
+        stroke("black");
+        strokeWeight(8);
+  text("Type The Following Word : ", 280, 90);
+  for (let i = 0; i < longer.length; i++) {
+        const char = opts.word.charAt(i);
+        const offset = textWidth(longer.substr(0, i));
+
+        const other = opts.buffer.charAt(i);
+
+        if (other && other !== char) {
+            fill("red");
+            text(other, 100 + offset, 280);
+            fill("red");
+        } else fill(other ? "green" : "white");
+
+        if (char) text(char, 100 + offset, 180);
+    }
+}
+
+function keyPressed() {
+    if (state !== STATE.TYPING)
+        return;
+
+    if (opts.showResults)
+        if (keyCode === ENTER || keyCode === RETURN)
+            if (--opts["# Rounds"])
+                initTyping();
+            else
+                setState(STATE.MENU);
+        else { }
+    else
+        if ("abcdefghijklmnopqrstuvwxyz".indexOf(key) !== -1) {
+            opts.buffer += key;
+            if (opts.buffer === opts.word)
+                opts.showResults = true;
+        } else if (keyCode === BACKSPACE)
+            opts.buffer = opts.buffer.substr(0, opts.buffer.length - 1);
+}
